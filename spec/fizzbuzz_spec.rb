0cb85822e9f70b08e091171f97f30c0d4510ac6b
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-06-09 10:02:37 +0000

    Added a test for multiple of 3 and amended the program

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe 'fizzbuzz' do
     it 'returns "fizzbuzz" when passed 15' do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
+    
+    if 'returns "fizzbuzz" when passed 1008' do
+        expect(fizzbuzz(1008)).to eq 'fizz'
+    end
 end
