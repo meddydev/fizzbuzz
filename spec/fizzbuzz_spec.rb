bf411b630976bc02db01fd3c4674afc583b19cb7
Ahmed <ahmed@hallajian.co.uk>
2022-06-09 11:08:47 +0100

    added another test for multiple of 5, amended program to pass test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,7 +13,11 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
     
-    if 'returns "fizzbuzz" when passed 1008' do
+    it 'returns "fizz" when passed 1008' do
         expect(fizzbuzz(1008)).to eq 'fizz'
+    end
+
+    it 'returns "buzz" when passed 5005' do
+        expect(fizzbuzz(5005)).to eq 'buzz'
     end
 end
